2c865b34488400f9a73d22d9515c458cc4b013e6
thomasneckel <thomasneckel@gmail.com>
2020-11-24 14:31:09 -0300

    Cinematica Inversa

---
+++ Control/brazo_robotico/src/brazo.h
@@ -0,0 +1,11 @@
+
+
+class Brazo{
+    public:
+        Brazo();
+        float L1;
+        float L2;
+        float L3;
+        std::tuple <float, float, float> Brazo::pos_motores(std::tuple <float, float, float> pos_obj);
+    private:
+};
